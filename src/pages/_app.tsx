158731adbcf15b3158d1bdb32b3bd05ffc452ef9
pprunty <pprunty@tcd.ie>
2024-06-01 13:24:31 +0200

    fix cache control

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,15 +42,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     updateMetaThemeColor(lightTheme.colorBackground);
 
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-          // todo: Update registration of path to be relative to domain: patrick.prunty.com/sw.js
-          navigator.serviceWorker
-            .register('./sw.js')
-            .then((registration) => {
-              console.log('Service Worker registered with scope:', registration.scope);
-            })
-            .catch((error) => {
-              console.error('Service Worker registration failed:', error);
-            })
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
     }
   }, []);
 
