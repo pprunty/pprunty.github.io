8906247cb64c6491b246bc5b34e9a5e24eb707d8
pprunty <pprunty@tcd.ie>
2024-06-19 12:01:29 +0100

    add google tag manager for analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import Script from 'next/script'
+import Script from 'next/script';
 
 class MyDocument extends Document {
-
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -36,9 +35,15 @@ class MyDocument extends Document {
           {/* Add your other head elements here */}
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content={'#F0F0F0'} />
+          <meta name="theme-color" content="#F0F0F0" />
+          {/* Google Tag Manager */}
           <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-LSBKKYLNC4`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
@@ -50,9 +55,7 @@ class MyDocument extends Document {
               `,
             }}
           />
-          {/*<meta name="google-adsense-account" content="ca-pub-2453030550493085"/>
-          <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2453030550493085"
-                         crossOrigin="anonymous"></Script>*/}
+          {/* End Google Tag Manager */}
         </Head>
         <body>
           <Main />
