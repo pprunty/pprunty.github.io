cecbb8034f830ac97386e4299c6a1c9fff45bfdb
pprunty <pprunty@tcd.ie>
2024-06-28 14:21:00 +0100

    update sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,12 +5,12 @@ const matter = require('gray-matter');
 function generateSitemap() {
   const postsDirectory = path.join(process.cwd(), 'posts');
   const filenames = fs.readdirSync(postsDirectory).filter(filename => /^\d/.test(filename));
-  console.log(filenames)
+  console.log(filenames);
 
   let sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
+  // Adding dynamic blog content
   filenames.forEach((filename) => {
     const filePath = path.join(postsDirectory, filename);
     const fileContents = fs.readFileSync(filePath, 'utf8');
@@ -24,11 +24,11 @@ function generateSitemap() {
     console.log(`Current date minus 2 days: ${currentDateMinus2Days}`);
 
     if (postDate > currentDateMinus2Days) {
-      console.log('in if statement')
+      console.log('in if statement');
       sitemapContent += `
   <url>
     <loc>https://patrickprunty.com/blog/${filename.replace(/\.md$/, '')}</loc>
-    <news:news>
+    <news:news xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
       <news:publication>
         <news:name>Patrick Prunty Blog</news:name>
         <news:language>en</news:language>
@@ -40,11 +40,29 @@ function generateSitemap() {
     }
   });
 
+  // Adding static pages
+  const staticPages = [
+    '',
+    'newsletter',
+    'photography',
+    'blog/page/1',
+    'project/jigsaw-academy',
+    'project/jigsaw-presents',
+    'videos'
+  ];
+
+  staticPages.forEach((page) => {
+    sitemapContent += `
+  <url>
+    <loc>https://patrickprunty.com/${page}</loc>
+  </url>`;
+  });
+
   sitemapContent += '\n</urlset>';
-  console.log(sitemapContent)
+  console.log(sitemapContent);
 
   fs.writeFileSync(path.join(process.cwd(), 'public', 'news-sitemap.xml'), sitemapContent);
-  console.log('News sitemap generated successfully.');
+  console.log('Sitemap generated successfully.');
 }
 
 generateSitemap();
