f331f7f0a3b026b129a823af740f88934fdd2ca9
pprunty <pprunty@tcd.ie>
2024-06-28 10:50:42 +0100

    update sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,6 +5,7 @@ const matter = require('gray-matter');
 function generateSitemap() {
   const postsDirectory = path.join(process.cwd(), 'posts');
   const filenames = fs.readdirSync(postsDirectory).filter(filename => /^\d/.test(filename));
+  console.log(filenames)
 
   let sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
@@ -15,7 +16,15 @@ function generateSitemap() {
     const fileContents = fs.readFileSync(filePath, 'utf8');
     const { data } = matter(fileContents);
 
-    if (new Date(data.date) > new Date(Date.now() - 2 * 24 * 60 * 60 * 1000)) {
+    const postDate = new Date(data.date);
+    const currentDateMinus2Days = new Date(Date.now() - 40 * 24 * 60 * 60 * 1000);
+
+    console.log(`Checking file: ${filename}`);
+    console.log(`Post date: ${postDate}`);
+    console.log(`Current date minus 2 days: ${currentDateMinus2Days}`);
+
+    if (postDate > currentDateMinus2Days) {
+      console.log('in if statement')
       sitemapContent += `
   <url>
     <loc>https://patrickprunty.com/blog/page/1/${filename.replace(/\.md$/, '')}</loc>
@@ -24,7 +33,7 @@ function generateSitemap() {
         <news:name>Patrick Prunty Blog</news:name>
         <news:language>en</news:language>
       </news:publication>
-      <news:publication_date>${new Date(data.date).toISOString()}</news:publication_date>
+      <news:publication_date>${postDate.toISOString()}</news:publication_date>
       <news:title>${data.title}</news:title>
     </news:news>
   </url>`;
@@ -32,6 +41,7 @@ function generateSitemap() {
   });
 
   sitemapContent += '\n</urlset>';
+  console.log(sitemapContent)
 
   fs.writeFileSync(path.join(process.cwd(), 'public', 'news-sitemap.xml'), sitemapContent);
   console.log('News sitemap generated successfully.');
